247132de4f67d9fcf2bb46a92b16ebe993a62f95
Ori Blanka <oriblanka305@gmail.com>
2021-09-30 19:37:14 +0300

    connect socket.io to server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,19 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
+const { Console } = require('console');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection");
+
+    socket.on('disconnect', () => {
+        console.log("User had left");
+    })
+});
 
 app.use(router);
 
