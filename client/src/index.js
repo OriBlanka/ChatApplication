7ead59440fdfda622be4521971250316b6fbdd5d
Ori Blanka <oriblanka305@gmail.com>
2021-09-30 19:22:12 +0300

    starting with creating a basic client components and starting building the server

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root')); //initialize root in the HTML -- react put all the context in the div="root" in index.html file in the root
