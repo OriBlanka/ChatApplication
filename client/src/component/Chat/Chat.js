3d6f88f21a54b55fcd70a506f506b383fdc532f7
Ori Blanka <oriblanka305@gmail.com>
2021-10-04 18:22:58 +0300

    Adding a connection between the client and the server sides  via emit method

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -2,13 +2,33 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string'; //Helping to use the url's parameters
 import io from 'socket.io-client';
 
-const Chat = ({ location }) => {
-    useEffect(() => {
-        const data = queryString.parse(location.search);
+let socket;
 
-        console.log(location.search);
-        console.log(data);
-    })
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+    
+        socket = io(ENDPOINT, { transports : ['websocket'] });
+    
+        setRoom(room);
+        setName(name)
+    
+        socket.emit('join', { name, room }, (error) => {
+          if(error) {
+            alert(error);
+          }
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+      }, [ENDPOINT, location.search]);
 
     return (
         <h1>Chat</h1>
